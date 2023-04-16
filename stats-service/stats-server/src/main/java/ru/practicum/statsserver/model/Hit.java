e85aa7c62d2ec5210456efa75e3723a60c81d44a
ldv2018 <ldv2018@yandex.ru>
2023-04-16 03:50:18 +0300

    fix hit repository

--- stats-service/stats-server/src/main/java/ru/practicum/statsserver/model/Hit.java
+++ stats-service/stats-server/src/main/java/ru/practicum/statsserver/model/Hit.java
@@ -2,6 +2,7 @@ package ru.practicum.statsserver.model;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import ru.practicum.statsdto.Stat;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -13,6 +14,47 @@ import java.time.LocalDateTime;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Builder(toBuilder = true)
 @Entity
+@NamedNativeQueries({
+        @NamedNativeQuery(name = "GetNotUniqueIpStat", resultSetMapping = "HitToDtoMapping",
+                query = "SELECT h.app AS app, h.uri AS uri, COUNT(h.ip) AS hits " +
+                        "FROM hits AS h " +
+                        "WHERE (h.req_time BETWEEN  :start AND :end) AND h.uri in :uris " +
+                        "GROUP BY h.app, h.uri " +
+                        "ORDER BY hits DESC"
+        ),
+        @NamedNativeQuery(name = "GetUniqueIpStat", resultSetMapping = "HitToDtoMapping",
+                query = "SELECT h.app AS app, h.uri AS uri, COUNT(DISTINCT h.ip) AS hits " +
+                        "FROM hits AS h " +
+                        "WHERE (h.req_time BETWEEN :start AND :end) AND h.uri in :uris " +
+                        "GROUP BY h.app, h.uri " +
+                        "ORDER BY hits DESC"
+        ),
+        @NamedNativeQuery(name = "GetNotUniqueIpNoUriStat", resultSetMapping = "HitToDtoMapping",
+                query = "SELECT h.app AS app, h.uri AS uri, COUNT(h.ip) AS hits " +
+                        "FROM hits AS h " +
+                        "WHERE (h.req_time BETWEEN :start AND :end) " +
+                        "GROUP BY h.app, h.uri " +
+                        "ORDER BY hits DESC"
+        ),
+        @NamedNativeQuery(name = "GetUniqueIpNoUriStat", resultSetMapping = "HitToDtoMapping",
+                query = "SELECT h.app AS app, h.uri AS uri, COUNT(DISTINCT h.ip) AS hits " +
+                        "FROM hits AS h " +
+                        "WHERE (h.req_time BETWEEN :start AND :end) " +
+                        "GROUP BY h.app, h.uri " +
+                        "ORDER BY hits DESC"
+        )
+})
+@SqlResultSetMapping(name = "HitToDtoMapping",
+        classes = {
+                @ConstructorResult(
+                        targetClass = Stat.class,
+                        columns = {
+                                @ColumnResult(name = "app", type = String.class),
+                                @ColumnResult(name = "uri", type = String.class),
+                                @ColumnResult(name = "hits", type = Integer.class)
+                        }
+                )}
+)
 public class Hit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- stats-service/stats-server/src/main/java/ru/practicum/statsserver/storage/StatsRepository.java
+++ stats-service/stats-server/src/main/java/ru/practicum/statsserver/storage/StatsRepository.java
@@ -12,32 +12,16 @@ import java.util.List;
 @Repository
 public interface StatsRepository extends JpaRepository<Hit, Integer> {
 
-    @Query("SELECT h.app AS app, h.uri AS uri, COUNT(h.ip) AS hits " +
-            "FROM Hit AS h " +
-            "WHERE (h.timestamp BETWEEN ?1 AND ?2) " +
-            "GROUP BY h.app, h.uri " +
-            "ORDER BY hits DESC")
+    @Query(name = "GetNotUniqueIpNoUriStat", nativeQuery = true)
     List<Stat> countHits(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT h.app AS app, h.uri AS uri, COUNT(h.ip) AS hits " +
-            "FROM Hit AS h " +
-            "WHERE (h.timestamp BETWEEN  ?1 AND ?2) AND h.uri in ?3 " +
-            "GROUP BY h.app, h.uri " +
-            "ORDER BY hits DESC")
+    @Query(name = "GetNotUniqueIpStat", nativeQuery = true)
     List<Stat> countHits(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("SELECT h.app AS app, h.uri AS uri, COUNT(DISTINCT h.ip) AS hits " +
-            "FROM Hit AS h " +
-            "WHERE (h.timestamp BETWEEN ?1 AND ?2) " +
-            "GROUP BY h.app, h.uri " +
-            "ORDER BY hits DESC")
+    @Query(name = "GetUniqueIpNoUriStat", nativeQuery = true)
     List<Stat> countHitsUnique(LocalDateTime start, LocalDateTime end);
 
-    @Query("SELECT h.app AS app, h.uri AS uri, COUNT(DISTINCT h.ip) AS hits " +
-            "FROM Hit AS h " +
-            "WHERE (h.timestamp BETWEEN ?1 AND ?2) AND h.uri in ?3 " +
-            "GROUP BY h.app, h.uri " +
-            "ORDER BY hits DESC")
+    @Query(name = "GetUniqueIpStat", nativeQuery = true)
     List<Stat> countHitsUnique(LocalDateTime start, LocalDateTime end, List<String> uris);
 
 }
